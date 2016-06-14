f30b71c1c0cd27940d1572c9c5006c152e4edbfb
Deepak Rohan Sekar <deepakrohan@hotmail.com>
2016-06-14 16:44:10 -0400

    Implementation of auth for routes

--- js/app.js
+++ js/app.js
@@ -2,22 +2,22 @@
  * Created by rohan on 6/11/16.
  */
 (function(){
-    angular.module("movieflix",['ngMessages','firebase','ngRoute'])
+ var app =   angular.module("movieflix",['ngMessages','firebase','ngRoute'])
             .config(function($routeProvider){
        $routeProvider
            .when('/',{
-               templateUrl: 'index.html',
+               templateUrl: 'login.html',
                controller: 'LoginController',
                controllerAs:'loginVm'
            })
            .when('/movieindex',{
-               templateUrl: 'movieindex.html',
+               templateUrl: 'movie.template.html',
                controller: 'MovieController',
                controllerAs:'movieVm',
                resolve:{
                    user: function (RegisterService) {
                         return RegisterService.getCurrentAuthInfo();
-               },
+                   },
                    moviesList:function (MovieService) {
                        return MovieService.getMoviesList();
                    },
@@ -26,8 +26,28 @@
                    },
                    moviesNowPlaying: function (MovieService) {
                        return MovieService.getNowPlaying();
-                   }
+                   },
+                   "currentAuth": ["Auth", function(Auth) {
+                       return Auth.$requireAuth();
+                   }]
                }
            })
     });
+
+    app.run(["$rootScope", "$location", function($rootScope, $location) {
+        $rootScope.$on("$routeChangeError", function(event, next, previous, error) {
+            // We can catch the error thrown when the $requireAuth promise is rejected
+            // and redirect the user back to the home page
+            if (error === "AUTH_REQUIRED") {
+                $location.path("/login.html");
+            }
+        });
+    }]);
+
+    app.factory("Auth", ["$firebaseAuth",
+        function($firebaseAuth) {
+            var ref = new Firebase("https://resplendent-heat-5158.firebaseIO.com");
+            return $firebaseAuth(ref);
+        }
+    ]);
 })()
