7a235811a4b0b511bf4113ec370145334cac4724
Deepak Rohan Sekar <deepakrohan@hotmail.com>
2016-06-14 03:30:26 -0400

    Feat: design of movie template

--- js/controller/movie.js
+++ js/controller/movie.js
@@ -1,3 +1,25 @@
 /**
  * Created by rohan on 6/13/16.
  */
+(function(){
+angular.module('movieflix').controller('MovieController',MovieController);
+
+    function MovieController(moviesList,topRatedList,moviesNowPlaying) {
+        var movieVm = this;
+        console.dir('Inside MovieController:');
+        movieVm.usersListT = moviesList.results;
+        console.log("Finally");
+        console.dir(moviesList.results)
+        var finalList = [];
+
+        // for( var i = 0 ;i < movieVmList.length ; i++){
+        //     finalList.push(movieVmList[i]);
+        // }
+        //
+        // console.log(finalList)
+        //
+        // for(var m in finalList){
+        //     console.log(m.adult)
+        // }
+    }
+})()
