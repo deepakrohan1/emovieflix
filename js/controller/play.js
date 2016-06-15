fe01fb5f737e7dc282e7bbd293e24399788f067c
Deepak Rohan Sekar <deepakrohan@hotmail.com>
2016-06-15 15:50:57 -0400

    design of movie view page

--- js/controller/play.js
+++ js/controller/play.js
@@ -24,11 +24,17 @@ function PlayControl(moviesList,topRatedList,moviesNowPlaying,$routeParams) {
         }
     }
     console.dir(playVm.totalMovies);
-
-    // console.log(totalMovies.length);
-    
     console.log($routeParams.id);
 
+    for(var j = 0 ; j < playVm.totalMovies.length; j++){
+        console.log(playVm.totalMovies[j].id );
+        if(parseInt(playVm.totalMovies[j].id) === parseInt($routeParams.id)){
+            console.log("if");
+            playVm.theMovie = playVm.totalMovies[j];
+        }
+    }
+
+    console.dir(playVm.theMovie);
 
 
 }
